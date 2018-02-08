1e27637f0e5f6ba7f4fc7bd8c54e92e392104f1c
Antti Räisälä <antti.raisala@vismaconsulting.fi>
2018-02-08 12:53:32 +0200

    Initial

---
+++ Assets/Thrust.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour {
+
+    [SerializeField]
+    private float thrust;
+    [SerializeField]
+	private Rigidbody rb;
+
+    [SerializeField]
+	private Vector3 previousPosition;
+    [SerializeField]
+	private float speed;
+
+    //private Transform transform;
+
+	void Start()
+	{
+        thrust = 20.0f;
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+	void FixedUpdate()
+	{
+        Vector3 currentPosition = new Vector3(0, transform.position.y, 0);
+        transform.position = currentPosition;
+		transform.rotation = Quaternion.identity;
+
+        // Calculate speed
+        if(previousPosition!=null){
+            Vector3 speedVector = currentPosition - previousPosition;
+            speed = speedVector.magnitude;
+        }
+        previousPosition = currentPosition;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * thrust);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
